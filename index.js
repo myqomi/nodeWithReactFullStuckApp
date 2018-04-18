860697806908bdfd353eee3a9c84d06086961bfa
Michael Mirski <mirski.mike@gmail.com>
2018-04-18 13:47:00 +0300

    changed greeding

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'Hello' });
 });
 
 const PORT = process.env.PORT || 5000;
-
-
 
 app.listen(PORT);

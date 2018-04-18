b97114a66963c41c9a2c09aa99c850c2587c0fd3
Michael Mirski <mirski.mike@gmail.com>
2018-04-18 14:16:24 +0300

    changed message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'Hello' });
+  res.send({ hi: 'Sivan' });
 });
 
 const PORT = process.env.PORT || 5000;

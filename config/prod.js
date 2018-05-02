e48da39e77eea104d34195102c66f499c97e8775
Michael Mirski <mirski.mike@gmail.com>
2018-05-02 16:57:58 +0300

    0002

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLINT_SECRET,
+  mongoURI: process.env.MONGO_URL,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+
